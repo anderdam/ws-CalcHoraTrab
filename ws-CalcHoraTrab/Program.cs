51bc72a349577c8d23fc8526b4d2c9fdab201c18
Anderson R Damasceno <28938242+anderdam@users.noreply.github.com>
2020-04-04 13:35:44 -0300

    Add project files.

---
+++ ws-CalcHoraTrab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ws_CalcHoraTrab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
